3b0f7c26f3ef92cabe5b9d4ed6938c48f373db27
amy <18301215242@139.com>
2019-03-15 16:52:43 +0800

    remove antd-pro

--- src/tool/common/Routes.js
+++ src/tool/common/Routes.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import WelcomePage from "../component/WelcomePage";
-import CustomCharts from "../../business/charts/CustomCharts";
+import ChartsPage from "../../business/charts/ChartsPage";
 import TableDemo from "../../business/tables/TableDemo";
 
 const Routes = () => (
   <Switch>
     <Route path="/" exact component={WelcomePage} />
-    <Route path="/charts/CustomCharts" component={CustomCharts} />
+    <Route path="/charts/CustomCharts" component={ChartsPage} />
     <Route path="/tables/TableDemo" component={TableDemo}/>
     <Route render={() => <h1>找不到此页面</h1>} />
   </Switch>
