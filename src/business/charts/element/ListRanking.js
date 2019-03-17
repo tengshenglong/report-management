dc977380ae4bef1f6845d71a98e69c19381462fb
amy <18301215242@139.com>
2019-03-17 19:32:45 +0800

    dashboard 样式

--- src/business/charts/element/ListRanking.js
+++ src/business/charts/element/ListRanking.js
@@ -3,9 +3,9 @@ import style from "../charts.module.less";
 import { Divider } from "antd";
 
 class ListRanking extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
+    this.state = {
       topListCity: [
         { title: "BACSKOKOOO", total: "34341223" },
         { title: "12121212", total: "34341223" },
@@ -13,40 +13,38 @@ class ListRanking extends Component {
         { title: "dsadasdasd", total: "34341223" },
         { title: "dsdasdasas", total: "34341223" }
       ]
-    }
+    };
   }
-  render(){
-    return <div className={style["salesRank"]}>
-      <h4 className={style["rankingTitle"]}>
-        物料销售TOP10(元)
-      </h4>
-      <ul className={style["rankingList"]}>
-        {this.state.topListCity.map((item, i) => (
-          <li key={item.title} style={{ height: "26px" }}>
-                        <span
-                          className={
-                            i < 3
-                              ? style["rankingItemNumberFirst"]
-                              : style["rankingItemNumber"]
-                          }
-                        >
-                          {i + 1}
-                        </span>
-            <span
-              className={style["rankingItemTitle"]}
-              title={item.title}
-            >
-                          {item.title}
-                        </span>
-            <Divider type="vertical" />
-            <span className={style["rankingItemValue"]}>
-                          {item.total}
-                        </span>
-          </li>
-        ))}
-      </ul>
-    </div>
+  renderSpan = i => {
+    if (i === 0) {
+      return <span className={style["rankingItemNumberFirst"]}>{i + 1}</span>;
+    } else if (i === 1) {
+      return <span className={style["rankingItemNumberSecond"]}>{i + 1}</span>;
+    } else if (i === 2) {
+      return <span className={style["rankingItemNumberThird"]}>{i + 1}</span>;
+    } else {
+      return <span className={style["rankingItemNumber"]}>{i + 1}</span>;
+    }
+  };
+  render() {
+    return (
+      <div className={style["salesRank"]}>
+        <h4 className={style["rankingTitle"]}>物料销售TOP10(元)</h4>
+        <ul className={style["rankingList"]}>
+          {this.state.topListCity.map((item, i) => (
+            <li key={item.title} style={{ height: "26px" }}>
+              {this.renderSpan(i)}
+              <span className={style["rankingItemTitle"]} title={item.title}>
+                {item.title}
+              </span>
+              <Divider type="vertical" />
+              <span className={style["rankingItemValue"]}>{item.total}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
   }
 }
 
-export default ListRanking
+export default ListRanking;
